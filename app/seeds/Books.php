88a03e10a4e9c60611df949ea2f947c82add44e5
Tate Peñaranda <tate@xenir.com.ar>
2017-02-24 22:51:20 -0300

    I'm moving forward with the "Virtual bookshelf" example, so I'm pushing a kind of seeder/migration for the 'books' table. No sense to code in order to handle migrations or proper seeding, this is a really simple MVC framework.

---
+++ app/seeds/Books.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+
+$config = include(dirname(dirname(__DIR__)).'/config/config.php');
+
+try {
+    $conn = new PDO(
+        "mysql:host={$config['database']['host']};dbname={$config['database']['database']}",
+        $config['database']['username'],
+        $config['database']['password']
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn->exec('DROP TABLE IF EXISTS books');
+
+    // sql to create table
+    $sql = "CREATE TABLE books (
+        `id` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        `title` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+        `author` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+        `created_at` timestamp NOT NULL DEFAULT NOW(),
+        `deleted_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+    )";
+
+    $conn->exec($sql);
+
+    echo "Table 'books' created successfully\n";
+
+    $conn->exec('INSERT INTO books (`title`, `author`) VALUES ("Test Title", "Test author")');
+
+    echo "Table 'books' seeded successfully\n";
+    }
+catch (PDOException $e) {
+    echo "Error creating/seeding 'books' table: {$e->getMessage()}\n";
+}
