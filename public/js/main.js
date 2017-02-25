d7220f20cc69bc7f35a53de396d6c9eea577383f
Tate Peñaranda <tate@xenir.com.ar>
2017-02-25 19:14:01 -0300

    Adding JS code to perform POST request when adding items.

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+$('.input-form').submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+            url: '',
+            type: 'POST',
+            traditional: true,
+            timeout: 3000,
+            data: $(this).serializeArray(),
+            success: function(data) {
+                i = data.data;
+                $('#messages').html('New item added!')
+                .addClass('green').removeClass('red').show().delay(1000).fadeOut();
+
+                var itemHtml = '<a href="/vinils/details/' + i.id + '" class="list-group-item">' +
+                    '<i>' + i.title + '</i> - ' + i.artist + ' - [' + i.genre + ']' +
+                    '</a>';
+
+                $('.list-group.vinils a').first().before(itemHtml);
+            },
+            error: function(e) {
+                $('#messages').html('There was an error addding the item!')
+                .addClass('red').removeClass('green').show().delay(3000).fadeOut();
+            }
+        });
+    });
