37f751a456e1ee900880017061c0197e96eda401
Tate Peñaranda <tate@xenir.com.ar>
2017-02-25 01:40:37 -0300

    Added error handling for missing views.

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -15,12 +15,17 @@ class BaseController
         $folderName = preg_replace(['/^morsummvc.controllers./', '/controller$/'], '', strtolower(get_class($this)));
 
         $viewName = strtolower(preg_replace('/^get/', '', debug_backtrace()[1]['function']));
+        $viewFile = dirname(dirname(__FILE__))."/views/{$folderName}/{$viewName}.view";
 
-        $view = file_get_contents(dirname(dirname(__FILE__))."/views/{$folderName}/{$viewName}.view");
+        $view = @file_get_contents($viewFile);
 
-        $mapFunction = function ($item) { return "/\{\{ $item \}\}/"; };
-        $filledView = preg_replace(array_map($mapFunction, array_keys($opts)), array_values($opts), $view);
+        if ($view) {
+            $mapFunction = function ($item) { return "/\{\{ $item \}\}/"; };
+            $filledView = preg_replace(array_map($mapFunction, array_keys($opts)), array_values($opts), $view);
 
-        echo $filledView;
+            echo $filledView;
+        } else {
+            echo "Can't read {$viewFile} file.";
+        }
     }
 }
