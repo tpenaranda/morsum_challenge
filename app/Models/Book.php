889a3e08db55c3a6330170f80c0343dd097e26aa
Tate Peñaranda <tate@xenir.com.ar>
2017-02-25 16:23:29 -0300

    Sorting.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,14 +7,14 @@ class Book extends \OurLittleORM
     public static $fillable = ['title', 'author'];
     public static $tableName = 'books';
 
-    public $id = null;
-    public $title = null;
     public $author = null;
     public $created_at = null;
+    public $id = null;
+    public $title = null;
 
     public function __construct(array $params = [])
     {
-        $this->title = empty($params['title']) ? false : $params['title'];
         $this->author = empty($params['author']) ? false : $params['author'];
+        $this->title = empty($params['title']) ? false : $params['title'];
     }
 }
